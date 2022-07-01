733108b360e7cb99fab5eaf3a9124efc78cc6544
abdelrhmany65 <90100454+abdelrhmany65@users.noreply.github.com>
2022-07-02 06:08:15 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn').onclick = () =>{
+    document.querySelector('.cookies-box').classList.add('active')
+}
+
+document.querySelector('.clos').onclick = () =>{
+    document.querySelector('.cookies-box').classList.remove('active')
+}
